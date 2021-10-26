79d29a76e61f05112c937e371edcb7602fa05a4c
caadams4 <79220528+caadams4@users.noreply.github.com>
2021-10-26 19:34:36 +0000

    need to fix prereqs ... this or that, this and that.
    need to only capture description plain text

--- main.py
+++ main.py
@@ -4,6 +4,158 @@ import json
 
 i = 270867
 
+
+def scrape(i):
+
+    URL = "https://catalog.udel.edu/preview_course.php?coid=" + str(i)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("td", class_="block_content_popup")
+    strResults = str(results)
+
+    #Title
+    try:
+        title = results.find(id="course_preview_title")
+        courseTitle = title.text
+    except Exception:
+        return
+
+    #CourseID -- parsed from title
+    courseID = title.text[:8]
+
+    #Credits(s):
+    startSearch = strResults.find('Credit(s):')
+    credits = strResults[startSearch + 20:startSearch + 21]
+
+    #Component:
+    startSearchTmp = strResults.find('Component:', startSearch)
+    endSearch = startSearch
+    if startSearchTmp != -1:
+        startSearch = startSearchTmp
+        endSearch = strResults.find('<', startSearch + 11)
+        component = strResults[startSearch + 20:endSearch]
+    else:
+        component = "Lecture"
+
+    #description
+    startSearchTmp = strResults.find('br', startSearch + 3)
+    cutoff = strResults.find('<strong>', startSearch + 3)
+    endSearchTmp = strResults.find('<br', startSearchTmp + 1)
+
+    if startSearchTmp != -1 or cutoff > startSearchTmp:
+        startSearch = startSearchTmp
+        endSearch = endSearchTmp
+        description = strResults[startSearch + 4:endSearch]
+    else:
+        description = "None"
+
+    #Repeatable for credit:
+    rfc = 'Repeatable for Credit:'
+    startSearchTmp = strResults.find('Repeatable for Credit:', startSearch)
+    endSearchTmp = strResults.find('<strong>', startSearch)
+    if startSearchTmp != -1:
+        startSearch = startSearchTmp
+        endSearch = endSearchTmp
+        repeatable = strResults[startSearch + len(rfc) + 10:endSearch]
+    else:
+        repeatable = "unspecified"
+
+    #Allowed Units:
+    au = 'Allowed Units:'
+    startSearch = strResults.find('Allowed Units:', startSearch)
+    endSearch = strResults.find('</strong>', startSearch)
+    allowedUnits = strResults[startSearch + len(au) + 10:endSearch]
+
+    #Multiple Term Enrollment:
+    startSearch = strResults.find('Multiple Term Enrollment:', startSearch)
+    endSearch = strResults.find('</strong>', startSearch)
+    multTermEnrollment = strResults[startSearch + len(au) + 21:endSearch]
+
+    #Search for crosslisted
+    crosslist = []
+    if strResults.find('Crosslisted:') != -1:
+        while startSearch != -1:
+            foundCrosslist = strResults.find('</a>', startSearch)
+            crosslistEnd = strResults.find('. ', foundCrosslist)
+            if foundCrosslist > crosslistEnd or foundCrosslist == -1:
+                break
+            startSearch = foundCrosslist
+            crosslist += [strResults[startSearch - 8:startSearch]]
+            startSearch += 1
+
+    #Search for PREREQs:
+    prereqs = []
+    startSearchTmp = strResults.find('PREREQ:', startSearch)
+    if startSearchTmp != -1:
+        startSearch = startSearchTmp
+        while startSearch != -1:
+            foundPrereq = strResults.find('</a>', startSearch)
+            prereqEnd = strResults.find('<hr', startSearch)
+            if foundPrereq > prereqEnd or foundPrereq == -1:
+                break
+            startSearch = foundPrereq
+            prereqs += [strResults[startSearch - 8:startSearch]]
+            startSearch += 1
+
+    univBreadth = ""
+    startSearchTmp = strResults.find('University Breadth:', startSearch)
+    if startSearchTmp != -1:
+        startSearch = startSearchTmp
+        foundUB = strResults.find('<strong>', startSearch+2)
+        univBreadth += strResults[startSearch + 28:foundUB-1]
+
+    engineeringBreadth = ""
+    startSearchTmp = strResults.find('College of Engineering Breadth:', startSearch)
+    if startSearchTmp != -1:
+        startSearch = startSearchTmp
+        foundEB = strResults.find('<strong>', startSearch+2)
+        engineeringBreadth += strResults[startSearch + 39:foundEB-1]
+
+
+    course = {  #build course JSON
+        courseID: {
+            'title': courseTitle,
+            "credits": credits,
+            "description": description,
+            "component": component,
+            "repeatable": repeatable,
+            "allowedUnits": allowedUnits,
+            "multTermEnrollment": multTermEnrollment,
+            "prereqs": prereqs,
+            "crosslisted": crosslist,
+            "University Breadth": univBreadth,
+            "Engineering Breadth": "",
+            "URL": URL
+        }
+    }
+    return course
+
+
+courses=[]
+while i < 275300:
+#while i < 270899: 
+  jsonScrape = scrape(i)
+  if jsonScrape :
+    courses += jsonScrape.items()
+
+  i+=1
+
+
+x = json.dumps(courses)
+file = open('courseData.json', 'w')
+file.write(x)
+file.close()
+
+
+
+
+"""
+import requests
+from bs4 import BeautifulSoup
+import json
+
+i = 270867
+
 def scrape(i):
 
   URL = "https://catalog.udel.edu/preview_course.php?coid=" + str(i)
@@ -41,7 +193,7 @@ def scrape(i):
     description = strResults[startSearch+4:endSearch]
     startSearch = startSearchTmp
     endSearch = endSearchTmp
-    print(description)
+   
 
 
   #Repeatable for credit:
@@ -93,21 +245,22 @@ def scrape(i):
       "URL" : URL
     }
   }
-
+  print(course)
   return course
-courses=[]
-while i < 275300:
+#courses=[]
+#while i < 275300:
 #while i < 270872: test
-  jsonScrape = scrape(i)
+#  jsonScrape = scrape(i)
+#  print(i)
+#  if jsonScrape :
+#    courses += jsonScrape.items()
 
-  if jsonScrape :
-    courses += jsonScrape.items()
-
-  i+=1
-  
+#  i+=1
+scrape(i)
 x = json.dumps(courses)
-print(x)
+print(i)
 
 file = open('courseData.json', 'w')
 file.write(x)
 file.close()
+"""
